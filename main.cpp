b70f9d30efe58809978755052d666a8b80f786e1
Habib <mosaddikhabib.bsfmstu@gmail.com>
2022-08-09 14:00:54 +0600

    public function added

--- main.cpp
+++ main.cpp
@@ -7,6 +7,26 @@ using namespace std;
 class contact {
      char fName[50], lName[50], address[100],email[50];
      long long phNo;
+
+     //public function to take input form user end
+ public:
+    void createContact()
+    {
+        cout<<"First Name: ";
+        cin>>fName;
+
+        cout<<"Last Name: ";
+        cin>>lName;
+
+        cout<<"Phone Number: ";
+        cin>>phNo;
+
+        cout<<"Address: ";
+        cin>>address;
+
+        cout<<"E-Mail address: ";
+        cin>>email;
+    }
 };
 
 int main ()
