3f41d896aea016ad0de0cf0f9a06ed5efe0e61e4
Habib <mosaddikhabib.bsfmstu@gmail.com>
2022-08-11 23:45:48 +0600

    welcome part edited

--- main.cpp
+++ main.cpp
@@ -160,8 +160,9 @@ int main ()
     system("cls");
     system("color 3F"); //here color XY means , X=bg color and Y = text color
 
-    cout<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<"\t\t\t\t\t Welcome to my Handmade phone-book management system"<<endl<<endl<<endl<<endl<<endl<<endl;
+    cout<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<"\t\t\t\t\t Welcome to my Handmade phone-book management system"<<endl<<"\t\t\t\t\t\t(Press any key to start...)"<<endl<<endl<<endl<<endl<<endl;
     getch();
+
 
     while(1){
         contact c1;
